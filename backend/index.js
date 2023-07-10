3497a1e58e170ea46853f44362c98f6b9d597ceb
elahemortazavi <emor5680@gmail.com>
2023-07-10 19:28:32 +0100

    connecting to database on render

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,39 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8800;
+
 // Database configuration
 const pool = new Pool({
   connectionString: process.env.DB_URL, // Connection URL for connecting to the PostgreSQL database
   ssl: {
-    rejectUnauthorized: true, // Disabling SSL/TLS certificate verification (for development purposes only)
+    rejectUnauthorized: true, 
   },
 });
 
+// app.get("/", (req, res) => {
+//   res.json("hello");
+// });
+
+// GET root
+app.get("/", (req, res) => {
+  // Delete this line after you've confirmed your server is running
+  res.send({ express: "Your Backend Service is Running" });
+});
+
+
+
+  // GET 
+app.get("/books", async (req, res) => {
+  try {
+    const query = `SELECT * FROM books`;
+    const result = await pool.query(query);
+    const books = result.rows;
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({err: "fetching error"})
+  }
+  
+});
 
 app.listen (port, () => {
     console.log(`connected to ${port}`)
